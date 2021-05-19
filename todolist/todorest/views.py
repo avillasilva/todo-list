4be6666be7743e4c8d25706f8471357f33d50462
Ávilla Silva <silvaavilla74@gmail.com>
2021-05-19 20:04:47 -0300

    register users

--- todolist/todorest/views.py
+++ todolist/todorest/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
+from django.http.response import Http404
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def register_user(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(request['username'], request['email'], request['password'])
+        user.save()
+        return HttpResponse(user.id)
+    else:
+        return HttpResponse(status=400)
