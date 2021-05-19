eb2e2676f5f7fe8e9ae522a3df7225efc869f192
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2021-05-19 19:36:48 -0300

    begin models

--- todolist/todorest/models.py
+++ todolist/todorest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField
+import datetime
 
-# Create your models here.
+class List(models.Model):
+    title = models.CharField(max_length=200, null=True,blank=True)
+    task = models.CharField(max_length=200, null=True,blank=True)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, null=True,blank=True)
+    deadline = models.DateTimeField(default=datetime.date.today)
+    description = models.CharField(max_length=200)
+
