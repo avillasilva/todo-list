407bdd8d9cb71a15bb3a1e88c73d1921f0dc3186
suria.mousinho <suria.mousinho@estagiario.ufca.edu.br>
2021-05-19 19:56:27 -0300

    Addition to models: Category

--- todolist/todorest/models.py
+++ todolist/todorest/models.py
@@ -8,9 +8,18 @@ class TaskList(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, null=True,blank=True)
 
+class Category(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True,blank=True)
+    description = models.CharField(max_length=200)
+
 class Task(models.Model):
     ownerList = models.OneToOneField(TaskList, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, null=True,blank=True)
     deadline = models.DateTimeField(default=date.today+timedelta(hours=6))
     description = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
 
