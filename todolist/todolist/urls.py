36e62146b41f391994535bf362ed6beb3fca8210
Ávilla Silva <silvaavilla74@gmail.com>
2021-06-15 20:02:32 -0300

    urls fixed and migrations applied

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todorest/', include('todorest.urls')),
-    path('notes/', include('notes.url'))
+    path('notes/', include('notes.urls'))
 ]
