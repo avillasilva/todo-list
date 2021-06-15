ee45ba10dc8c11e97b5dc249cd3f14d76be2974a
Ávilla Silva <silvaavilla74@gmail.com>
2021-06-15 19:33:03 -0300

    notes urls

---
+++ todolist/notes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+app_name = 'notes'
+urlpatterns = [
+    path('notes/', views.create_or_get_notes, name='create_or_get_notes'),
+    path('notes/<int:note_id>', views.crud_notes, name='crud_note'),
+]
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todorest/', include('todorest.urls'))
+    path('todorest/', include('todorest.urls')),
+    path('notes/', include('notes.url'))
 ]
