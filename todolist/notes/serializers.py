3cf3471d86fe677c8c55d647bacec77e675c36b6
Victor Koehler <koehlervictor@cc.ci.ufpb.br>
2021-06-16 21:45:53 -0300

    Serializer Integrity Check

--- todolist/notes/serializers.py
+++ todolist/notes/serializers.py
@@ -5,7 +5,11 @@ import json
 def note_encoder(note, request=None):
     tl = note.tasklist
     if request:
-        tl = json.loads(TaskList_Proxy.get(request, tl).content.decode('utf-8'))
+        tlj = TaskList_Proxy.get(request, tl)
+        if tlj.status_code < 400:
+            tl = json.loads(tlj.content.decode('utf-8'))
+        else:
+            raise Exception('Not allowed')
     return {'id': note.id, 'title': note.title,
             'content': note.content,
             'tasklist': tl, 'owner': note.owner.username}
