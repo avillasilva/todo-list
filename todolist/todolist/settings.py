a5c1e5dc0eaf7fae00b48834b4bc1a623de1d315
Victor Koehler <koehlervictor@cc.ci.ufpb.br>
2021-07-16 11:58:48 -0300

    Content-Type CORS Header

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -76,6 +76,8 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000','http://localhost:3000',)
 CORS_ALLOW_HEADERS = [
+    'Content-Type',
+    'Origin',
     'token',
     'token-notes'
 ]
